8f39a419b195b82377cd833bf1149c627a4a3b1b
Shawn Sprehe <memakeit@gmail.com>
2010-05-25 17:30:19 -0600

    removed piwil provider. added comments

--- classes/controller/test/analytics.php
+++ classes/controller/test/analytics.php
@@ -1,21 +1,36 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
+/**
+ * Analytics test controller.
+ *
+ * @package     MMI Analytics
+ * @author      Me Make It
+ * @copyright   (c) 2010 Me Make It
+ * @license     http://www.memakeit.com/license
+ */
 class Controller_Test_Analytics extends Controller
 {
+    /**
+     * @var boolean turn debugging on?
+     **/
     public $debug = TRUE;
 
+    /**
+     * Debug the Clicky analytics output.
+     *
+     * @return  void
+     */
     public function action_clicky()
     {
-        MMI_Debug::dump(Request::factory('analytics/clicky')->execute()->response, 'analytics/clicky');
+        MMI_Debug::dump(Request::factory('mmi/analytics/clicky')->execute()->response, 'analytics/clicky');
     }
 
+    /**
+     * Debug the Google analytics output.
+     *
+     * @return  void
+     */
     public function action_google()
     {
-        MMI_Debug::dump(Request::factory('analytics/google')->execute()->response, 'analytics/google');
+        MMI_Debug::dump(Request::factory('mmi/analytics/google')->execute()->response, 'analytics/google');
     }
-
-    public function action_piwik()
-    {
-        MMI_Debug::dump(Request::factory('analytics/piwik')->execute()->response, 'analytics/piwik');
-    }
-} // End Test Analytics
+} // End Controller_Test_Analytics
