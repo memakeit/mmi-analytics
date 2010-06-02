867fd76008f620f877bf02e354102f680aabdf42
Shawn Sprehe <memakeit@gmail.com>
2010-06-02 10:51:32 -0600

    added JSMin vendor files

---
+++ vendor/jsmin/jsmin_required.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Include the Javascript minify file.
+ *
+ * @package     JSMin
+ * @link        http://github.com/rgrove/jsmin-php/
+ */
+
+require_once dirname(__FILE__).'/JSMin.php';
