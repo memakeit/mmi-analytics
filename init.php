dcbbede7d96e5b4c7c0975fab2689568c3a9088f
Shawn Sprehe <memakeit@gmail.com>
2010-07-04 12:13:12 -0600

    added init file

---
+++ init.php
@@ -0,0 +1,20 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// MMI route (used for internal requests)
+Route::set('mmi/analytics', 'mmi/analytics(/<action>(/<id>))')
+->defaults(array
+(
+	'controller'	=> 'analytics',
+	'directory'		=> 'mmi',
+));
+
+// Test route
+if (Kohana::$environment !== Kohana::PRODUCTION)
+{
+	Route::set('test/analytics', 'test/analytics(/<action>)')
+	->defaults(array
+	(
+		'controller'	=> 'analytics',
+		'directory'		=> 'test',
+	));
+}
