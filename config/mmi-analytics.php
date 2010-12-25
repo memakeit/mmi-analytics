18b803808fafa782aaa686367b443f8b8c0101cc
Shawn Sprehe <memakeit@gmail.com>
2010-12-25 13:57:44 -0600

    updated config. added include_script_tag parameter.

--- config/mmi-analytics.php
+++ config/mmi-analytics.php
@@ -3,16 +3,27 @@
 // MMI analytics configuration
 return array
 (
-	'clicky' => array
+	'include_script_tag' => TRUE,
+	'services' => array
 	(
-		'asynchronous'	=> TRUE,	// default = TRUE
-		'id'			=> 123456,
-		'minify'		=> FALSE,	// default = FALSE
-	),
-	'google' => array
-	(
-		'asynchronous'	=> TRUE,	// default = TRUE
-		'id'			=> 'UA-XXXXXXX-X',
-		'minify'		=> FALSE,	// default = FALSE
+		MMI_Analytics::CLICKY => array
+		(
+			'asynchronous'	=> TRUE,	// default = TRUE
+			'id'			=> 123456,
+			'minify'		=> FALSE,	// default = FALSE
+		),
+		MMI_Analytics::GOOGLE => array
+		(
+			'asynchronous'	=> TRUE,
+			'id'			=> 'UA-XXXXXXX-X',
+			'minify'		=> FALSE,
+		),
+		MMI_Analytics::PIWIK => array
+		(
+			'asynchronous'	=> TRUE,
+			'id'			=> 123456,
+			'minify'		=> FALSE,
+			'url'			=> 'localhost/analytics/piwik',
+		),
 	),
 );
